b928a9bb22d24151c71e8e8bf751903586d3b908
Kyung Myung(Andy) Ko <40922371+andyko208@users.noreply.github.com>
2026-07-23 17:52:28 -0700

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/reverse-string/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # one ptr 0 and the other len(s)-1
+        # another ptr temp to replace one after another
+        # run it while one ptr < len(s)-1
+        # odd len string will not need to swap the center element
+        l = 0
+        r = len(s)-1
+        # temp = None
+        while l < r:
+            # temp = s[l]
+            s[l], s[r] = s[r], s[l]
+            # s[l] = s[r]
+            # s[r] = temp
+            l += 1
+            r -= 1
+        return s
+        
