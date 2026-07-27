f8c8575c2e565597828e7576b959cedb52d1f370
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-07-27 09:43:33 -0700

    Add: assign-cookies - submission-5

---
+++ Data Structures & Algorithms/assign-cookies/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        # Objective: maximize # of content children = how can we maximize distribution of
+        #               limited # of cookies to each of children with lowest g?
+
+        # Return: max number of content children
+        # two pointers, l and r to compare each other
+        # loop until l < len(g) or r < len(s), increment counter is g[l] == s[r]
+        l, r = 0, 0, 
+        g.sort()
+        s.sort()
+        while l < len(g) and r < len(s):
+            if g[l] <= s[r]:
+                l += 1
+            r += 1
+        return l
+# [1, 2, 2, 3, 10]
+# [1, 2, 3, 4]
