bc0e226dedccf5cdc2e83f4f7ba45b7b9ab63bcc
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-08-02 21:21:32 -0700

    Add: rotate-array - submission-4

---
+++ Data Structures & Algorithms/rotate-array/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # # make a copy of nums to iterete through
+        # cop = nums.copy()
+        # # modify nums in place by iterating through the copy, adding up k from i
+        # for i in range(len(cop)):
+        #     ind = i + k % len(nums)
+        #     if ind >= len(nums):
+        #         ind -= len(nums)
+        #     nums[ind] = cop[i]
+        k %= len(nums)
+        l, r = 0, len(nums)-1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+        l, r = 0, k-1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+        
+        l, r = k, len(nums)-1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+        
