f8b2532a9d2303d523d98d74ac7122432014e412
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-08-05 10:10:37 -0700

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, h = 0, len(nums)
+        while l < h:
+            mid = (l + h) // 2
+            # print(nums[l], nums[mid], nums[h])
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid+1
+            else:
+                h = mid
+        return -1
+        
+        # base case: if the value == target, return
+        # search(l+1, h) if value is smaller than target
+        # search(0, l) if vallue is larger than target
+        
+        # -1, 4, 8
+        # -1, 0, 2
+        #  4, 4, 4
+        #  4, x, 2
