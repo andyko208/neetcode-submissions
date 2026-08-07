e91b8035b0965834d27da40312cfb25dbaabebd3
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-08-07 10:02:52 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_hash, t_hash = {}, {}
+        for c in s:
+            if c in s_hash:
+                s_hash[c] += 1
+            else:
+                s_hash[c] = 1
+        for c in t:
+            if c in t_hash:
+                t_hash[c] += 1
+            else:
+                t_hash[c] = 1
+        print(s_hash, t_hash)
+    
+        return s_hash == t_hash
