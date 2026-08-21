c892252542f80d58d8349a1de55ebbee24e4401e
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-08-20 22:15:15 -0700

    Add: contains-duplicate-ii - submission-7

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-7.py
@@ -0,0 +1,42 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # # Naive O(N * k) solution
+        # # iterate i from 0 to n-k
+        # n = len(nums)
+        # for i in range(n-1):
+        #     # check for each i by iterating from i to i+k whether nums[i] == nums[j]
+        #     for j in range(i+1, i+k+1):
+        #         if j < n and nums[i] == nums[j]:
+        #             return True
+        # return False
+        # O(N)
+        # create a hashmap and store each element with indicies
+        # whenever new element is seen, check if that value - k is the previous ind to return True
+        # n = len(nums)
+        # mapp = {}
+        # for i in range(n):
+        #     if nums[i] in mapp and i - k <= mapp[nums[i]]:
+        #         return True
+        #     mapp[nums[i]] = i
+        # return False
+
+        # sliding window approach
+        # maintain a hashset window of size k
+        n = len(nums)
+        # keep two pointers L and R
+        L = 0
+        window = set()
+        # if R - L > k, remove L from the hashset and add R
+        for R in range(n):
+            if R - L > k:
+                window.remove(nums[L])
+                L += 1
+            if nums[R] in window:
+                return True
+            window.add(nums[R])
+        return False
+        # after adding the element, if the size of the hashset is < k, return True
+
+
+                
+        
