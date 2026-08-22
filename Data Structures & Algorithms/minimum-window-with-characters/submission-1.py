cb53caaa84a85a5fe1f47e5db593d200d90a90c8
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-08-22 14:04:23 -0700

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # Time: O(N), Memory: O(N)
+        # If t is empty, return "".
+        if t == "":
+            return ""
+
+        countT, window = {}, {}
+        # Build a frequency map countT for characters in t.
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+        print(countT)
+
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+
+        for r in range(len(s)):
+            # Add s[r] to window.
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            # If s[r] is in countT and its count in window matches countT, increment have.
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            # window is valid
+            while have == need:
+                # Update the best result if the current window is smaller.
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                # Then shrink from the left
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else ""
