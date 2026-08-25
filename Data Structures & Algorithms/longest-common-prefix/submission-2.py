ec80c1dd2ffcc552f2db3f5fc1bdda3d8abc070f
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-08-24 17:37:03 -0700

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # Brute force approach: get the smallest length string O(N^2)
+        # iterate through each strings to ensure the ith character of it matches with the rest
+        
+        res = ""
+        # get the minimum length string
+        min_ind = 0
+        n = len(strs)
+        for i in range(n):
+            if len(strs[i]) < len(strs[min_ind]):
+                min_ind = i
+        count = 0
+        # iterate through the minimum length string
+        for i in range(len(strs[min_ind])):
+            # check for all the rest of the strings
+            for j in range(n):
+                if strs[j][i] != strs[min_ind][i]:
+                    return res
+                count += 1
+            if count == n:
+                res += strs[min_ind][i]
+            count = 0
+        return res
