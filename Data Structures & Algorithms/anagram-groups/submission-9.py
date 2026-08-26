3ae717fb255dfa54b515bca890d23a017df4a997
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-08-26 10:08:47 -0700

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # create a hashmap of list to group anagrams
+        # set key as count list with 26 length -> O(1)
+        # append strs[i] with same key as list
+
+        # create a defaultdict list -> O(N)
+        str_map = defaultdict(list)
+        # iterate through s in strs
+        for s in strs:  # -> O(N)
+            # create a count map of list length 26
+            count = [0] * 26 # O(M) memory
+            # iterate through c in s
+            for c in s: # O(M) memory
+                count[ord(c) - ord('a')] += 1
+            # hashmap[count].append(s)
+            # print(count)
+            str_map[tuple(count)].append(s)
+        # return hashmap.values()
+        return list(str_map.values())   # O(N)
+            
