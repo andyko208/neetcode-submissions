f6cec7d854ac45814b1ffe9ac3af9571e281091a
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-09-03 13:14:45 -0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # return Counter(s) == Counter(t)
+        # create two hash maps and track each character with count
+        s_hash = defaultdict(int)
+        t_hash = defaultdict(int)
+        if len(s) != len(t):
+            return False
+        # iterate i through len(s)
+        for i in range(len(s)):
+            # add each char and count to the hashmap
+            s_hash[s[i]] += 1
+            t_hash[t[i]] += 1
+        return s_hash == t_hash
