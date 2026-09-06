3467ed6136556402290e6b96d68b1f54ab6615a2
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-09-06 12:54:40 -0700

    Add: string-matching-in-an-array - submission-8

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        # create a hashset of words
+        res = set()
+        # iterate through the words hashset and check if current word is in the each word of hashset
+        # Time: O(N^2), Space: O(N)
+        for w1 in words:
+            for w2 in words:
+                if w1 == w2:
+                    continue
+                elif w1 in w2:
+                    res.add(w1)
+                    break
+        return list(res)
+
+            
