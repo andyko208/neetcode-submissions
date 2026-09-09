0e8ed6928892ee7229a2a6ae2b1b850d0aa076a6
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-09-08 20:23:42 -0700

    Add: two-integer-sum-ii - submission-13

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-13.py
@@ -0,0 +1,45 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # Brute force approach: O(N^2)
+        n = len(numbers)
+        # # iterate a loop i from 0 to n
+        # for i in range(n):
+        #     # iterate a loop j from i+1 to n
+        #     for j in range(i+1, n):
+        #     # check if they sum up to target    
+        #         if numbers[i] + numbers[j] == target:
+        #             return [i+1, j+1]
+        # # Optimized approach: use hashmap
+        # # create a hashmap
+        # nums_map = {}
+        # n = len(numbers)
+        # # iterate i from 0 to n 
+        # for i in range(n):
+        #     # map target - numbers[i] to i
+        #     if numbers[i] in nums_map:
+        #         return [nums_map[numbers[i]]+1, i+1]
+        #     nums_map[target - numbers[i]] = i
+        #     # check if numbers[i] in nums_map and return [nums_map[numbers[i]], i]
+        # Space-optimized approach: binary search
+        # iterate i from 0 to n
+        # for each numbers[i], binary search target - numbers[i]
+        def search(l, h, goal):
+            while l <= h:
+                mid = (l + h) // 2
+                val = numbers[mid]
+                if val == goal:
+                    return mid
+                elif val < goal:
+                    l = mid + 1
+                else:
+                    h = mid - 1
+            return -1
+        n = len(numbers)
+        for i in range(n):
+            ind = search(0, n-1, target - numbers[i])
+            if ind != -1 and ind != i:
+                return [min(i+1, ind+1), max(i+1, ind+1)]
+
+
+
+
