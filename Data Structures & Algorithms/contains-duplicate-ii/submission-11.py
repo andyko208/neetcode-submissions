b820e45089489091933ffe8fe7d3599ea14cf4ff
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-09-17 09:56:57 -0700

    Add: contains-duplicate-ii - submission-11

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-11.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # Brute force
+        n = len(nums)
+        # iterate i from 0 to n-k
+        for i in range(n):
+            # iterate j from i+1 to i+k+1
+            window = min(n, i+k+1)
+            # print(window)
+            for j in range(i+1, window):
+                # check for nums[i] == nums[j] to return True
+                if nums[i] == nums[j]:
+                    return True
+        # return False
+        return False
