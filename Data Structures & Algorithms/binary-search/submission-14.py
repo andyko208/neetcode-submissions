d21020ed4a64dbdc2ae17f6f42f5cefe06051ae4
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-09-18 14:14:20 -0700

    Add: binary-search - submission-14

---
+++ Data Structures & Algorithms/binary-search/submission-14.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, h = 0, len(nums)-1
+        # while l < r
+        while l <= h:
+            # get the mid point = (l + h) / 2
+            mid = (l + h) // 2
+            # if num[mid] == target, return mid
+            if nums[mid] == target:
+                return mid
+            # elif nums[mid] < target, l = mid
+            elif nums[mid] < target:
+                l = mid + 1
+            # elif num[mid] > target, h = mid
+            elif nums[mid] > target:
+                h = mid - 1
+        # return -1
+        return -1
