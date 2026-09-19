defbcbb40c8b630ed9817b8a9fb01eea194b045f
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-09-19 10:44:30 -0700

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # have a pointer l and r that points to 0 and n-1
+        l, r = 0, len(s)-1
+        # iterate through while l < r while checking chars at l and r are alphanum and lowercase them
+        # s = s.lower()
+        while l < r:
+            # move up each pointers to check between the valid chars
+            if not s[l].isalnum():
+                l += 1
+                continue
+            if not s[r].isalnum():
+                r -= 1
+                continue
+            # return False whenever there's a mismatch, otherwise return True
+            if s[l].lower() != s[r].lower():
+                return False
+            # move up each pointers after checking
+            l, r = l + 1, r - 1
+        return True
