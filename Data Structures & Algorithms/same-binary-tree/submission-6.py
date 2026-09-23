04a83f8c2e1e6c69e5b5ba04246dd1e638d0b95f
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-09-22 21:18:14 -0700

    Add: same-binary-tree - submission-6

---
+++ Data Structures & Algorithms/same-binary-tree/submission-6.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # # BFS without additional list
+        # if not q and not p:
+        #     return True
+        # qp = deque([q])
+        # pp = deque([p])
+        # while qp and pp:
+        #     for _ in range(len(pp)):
+        #         nodeQ = qp.popleft()
+        #         nodeP = pp.popleft()
+        #         if nodeP is None and nodeQ is None:
+        #             continue
+        #         elif nodeP is None and nodeQ is not None:
+        #             return False
+        #         elif nodeP is not None and nodeQ is None:
+        #             return False
+        #         elif nodeP.val != nodeQ.val:
+        #             return False
+        #         qp.append(nodeQ.left)
+        #         qp.append(nodeQ.right)
+        #         pp.append(nodeP.left)
+        #         pp.append(nodeP.right)
+        # return True
+
+        if not p and not q:
+            return True
+        elif p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
+    
