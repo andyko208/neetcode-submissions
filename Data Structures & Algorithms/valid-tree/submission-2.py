07c73edd5b1daecd8c541f26ab8960d6c8f35957
Andy Ko <40922371+andyko208@users.noreply.github.com>
2026-09-23 17:36:05 -0700

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,51 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # tree is a graph with no cycle
+        # detect a cycle from the adjacency list
+
+        # create a Node class with val and neighbor nodes
+        
+        if len(edges) > n-1:
+            return False
+        # build adjacency list first
+        adj = [[] for _ in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        # DFS
+        visit = set()
+        def dfs(cur, par):
+            # cycle detected
+            if cur in visit:
+                return False
+            visit.add(cur)
+            for ne in adj[cur]:
+                # avoid treating traversal cycle
+                if ne == par:
+                    continue
+                # cycle detected
+                if not dfs(ne, cur):
+                    return False
+            return True
+        return dfs(0, -1) and n == len(visit)
+        # BFS
+        # visit = set()
+        # visit.add(0)
+        # # current node, parent node
+        # q = deque([(0, -1)])
+        # while q:
+        #     cur, par = q.popleft()
+        #     # visit the neighbors of cur
+        #     for nei in adj[cur]:
+        #         # avoid treating traversal as cycle
+        #         if nei == par:
+        #             continue
+        #         # cycle detected
+        #         if nei in visit:
+        #             return False
+        #         visit.add(nei)
+        #         q.append((nei, cur))
+        # return len(visit) == n
+
+        
